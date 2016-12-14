d02bdc5a271ab5694377c902999ffe23aa1e02be
Era <erasinbandith@gmail.com>
2016-12-14 10:23:58 -0500

    Connexion configuration

--- dao/bdd.php
+++ dao/bdd.php
@@ -2,8 +2,21 @@
 
 	try{
 
-		$bdd = new PDO('mysql:host=localhost;dbname=application_budget;charset=utf8', 'root', '',
-						array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+		$server = $url["host"];
+		$username = $url["user"];
+		$password = $url["pass"];
+		$db = substr($url["path"], 1);
+
+		if(empty($url)){
+			$server = "localhost";
+			$username = "root";
+			$password = "";
+			$db = "application_budget";
+		}
+
+		$bdd = new PDO('mysql:host='.$server.';dbname='.$db.';charset=utf8', $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}
 	catch (Exception $e){
         die('Erreur : ' . $e->getMessage());
